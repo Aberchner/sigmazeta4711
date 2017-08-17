7b7760a727bec065fdabaf7e6a0a805dbd678f89
Alexander Fink <afink01@students.uni-mainz.de>
2017-08-17 19:16:41 +0200

    Git regt mich auf

--- code/dataformatting.py
+++ code/dataformatting.py
@@ -10,25 +10,14 @@ Created on Thu Aug 17 10:56:28 2017
 
 data = []
 
-#file1 = open("comex.GC_140101_150101.csv")
-for line in file1:
+sourcefile = open("comex.GC_160101_170101.csv")
+destinfile = open("goldprize 2007-2017.txt", "a")
+
+for line in sourcefile:
     tmp = line.split(",")
     data.append((int(tmp[2]), int(tmp[3]), float(tmp[4]), float(tmp[7]), 
                      float(tmp[5]), float(tmp[6])))
 
-#file2 = open("comex.GC_150101_160101.csv")
-for line in file2:
-    tmp = line.split(",")
-    data.append((int(tmp[2]), int(tmp[3]), float(tmp[4]), float(tmp[7]), 
-                     float(tmp[5]), float(tmp[6])))
-
-#file3 = open("comex.GC_160101_170101.csv")
-for line in file3:
-    tmp = line.split(",")
-    data.append((int(tmp[2]), int(tmp[3]), float(tmp[4]), float(tmp[7]), 
-                     float(tmp[5]), float(tmp[6])))
-
-#file4 = open("goldprize 2014-2017.txt", "w")
 for i in range(len(data)):
-    file4.write(str(data[i]) + "\n")
-file4.close()
+    destinfile.write(str(data[i]) + "\n")
+destinfile.close()
