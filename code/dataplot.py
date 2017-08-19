d3a736ed930a5168cd246a51772122563be05c57
Alexander Fink <afink01@students.uni-mainz.de>
2017-08-19 20:52:20 +0200

    dataplot editiert

--- code/dataplot.py
+++ code/dataplot.py
@@ -14,16 +14,25 @@ src = open("goldprize 2007-2017.txt")
 # The method read_and_format gets the adress from a datafile, casts the
 # entries to ints/floats and groups them into an array, which is returned.
 
-def read_and_format(sourcefile):
+def read_and_format(sourcefile, N=np.Inf):
     array = []
+    i = 0
     for line in sourcefile:
         tmp = line[1:-2].split(", ")
         array.append((int(tmp[0]), int(tmp[1]), float(tmp[2]), float(tmp[3]), 
                       float(tmp[4]), float(tmp[5])))
+        i = i + 1
+        if i == N:
+            break
     return array
 
-data = read_and_format(src)
-print(data[0])
+N = 3077284
+data = read_and_format(src, N)
 
-#fig, axes = plt.subplots(figsize = (18, 9))
-#axes.plot(x, y)
+time = np.arange(0, N)
+value = []
+for i in range(len(data)):
+    value.append(data[i][2])
+
+fig, axes = plt.subplots(figsize = (18, 9))
+axes.plot(time, value)
