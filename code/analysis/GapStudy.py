343d7bbd34d88e4e1492bb249ecf043830316fd8
jascha Bohne <jaschabohne@web.de>
2017-08-23 01:17:07 +0200

    fixed formatting and style of file

--- code/analysis/GapStudy.py
+++ code/analysis/GapStudy.py
@@ -18,10 +18,10 @@ maxYear = 2017
 existingData = 0
 missingData = 0
 # all variables that base gaps on time use the time from the previous TradeData
-gapsByYear = [0]*(maxYear-minYear)
-gapsByMonth = [0]*12
-gapsByDay = [0]*7
-gapsByTime = [0]*(24*60)
+gapsByYear = [0] * (maxYear - minYear)
+gapsByMonth = [0] * 12
+gapsByDay = [0] * 7
+gapsByTime = [0] * (24 * 60)
 gapsByPrevVolume = {}
 amountByPrevVolume = {}
 gapsByPostVolume = {}
@@ -29,7 +29,7 @@ amountByPostVolume = {}
 gapLengths = {}
 
 
-def check_trade_data(tradeData, prevTradeData):
+def check_trade_data(trade_data, prev_trade_data):
     global existingData
     global missingData
     global gapsByYear
@@ -44,51 +44,54 @@ def check_trade_data(tradeData, prevTradeData):
     existingData += 1
 
     # calculate gap size
-    gapSize = ((tradeData.time - prevTradeData.time) / timedelta(minutes=1)) - 1
+    gap_size = ((trade_data.time - prev_trade_data.time) / timedelta(minutes=1)) - 1
     # count the number of gaps
-    missingData += gapSize
+    missingData += gap_size
     # count gaps by year
-    gapsByYear[(prevTradeData.time.year) - minYear] += gapSize
+    gapsByYear[prev_trade_data.time.year - minYear] += gap_size
     # count gaps by month
-    gapsByMonth[(prevTradeData.time.month)-1] += gapSize
+    gapsByMonth[prev_trade_data.time.month - 1] += gap_size
     # count Gaps by Weekday (Monday to Sunday)
-    gapsByDay[(prevTradeData.time.weekday())] += gapSize
+    gapsByDay[prev_trade_data.time.weekday()] += gap_size
     # count gaps by hours*60+minutes
-    gapsByTime[(prevTradeData.time.hour*60
-                + prevTradeData.time.minute)] += gapSize
+    gapsByTime[(prev_trade_data.time.hour * 60
+                + prev_trade_data.time.minute)] += gap_size
     # count gaps by the trade volume of the previous trade
-    #TODO
+    # TODO
     # count gaps by the trade volume of the following trade
-    #TODO
+    # TODO
     # count the Amount of gaps of each size
-    #TODO
+    # TODO
+
 
 def print_results():
     print("Number of TradeData: {0} Number of Gaps: {1} Percentage of missing Trades: {2}"
           .format(str(existingData), str(missingData), str(missingData / (missingData + existingData))))
-    #TODO
+    # TODO
 
 
 def write_results():
-    outputFile = open("../../textdocuments/gapStudyResults.txt", "w")
-    outputFile.write("existing: {0}; missing: {1}; {2}"
-                     .format(str(existingData), str(missingData), str(missingData / (missingData + existingData))))
-    #TODO
+    output_file = open("../../textdocuments/gapStudyResults.txt", "w")
+    output_file.write("existing: {0}; missing: {1}; {2}"
+                      .format(str(existingData), str(missingData), str(missingData / (missingData + existingData))))
+    # TODO
+
 
 def gap_study():
-    #setup
-    allData = dataformatting.read_comex_GC("../../data/comex.GC_070101_080101.csv")
+    # setup
+    all_data = dataformatting.read_comex_GC("../../data/comex.GC_070101_080101.csv")
 
-    print("reading finished - read " + str(len(allData)) + " lines")
+    print("reading finished - read " + str(len(all_data)) + " lines")
 
     # iterate over all TradeData and analyse Gaps
     prev = None
-    for currentTradeData in allData:
-        if not prev is None:
+    for currentTradeData in all_data:
+        if prev is not None:
             check_trade_data(currentTradeData, prev)
         prev = currentTradeData
 
     print_results()
     write_results()
+
 
 gap_study()
